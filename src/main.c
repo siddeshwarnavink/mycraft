aa6dd2cad93fad5377d234af7ee2149a21ba5676
Siddeshwar <siddeshwar.work@gmail.com>
2025-02-15 12:45:36 +0530

    fixing all fuckups

--- src/main.c
+++ src/main.c
@@ -7,14 +7,15 @@
 #define WORLD_BREADTH 20
 #define WORLD_HEIGHT 6
 
-// 0 - void
-// 1 - bedrock
-// 2 - stone
-// 3 - dirt
-// 4 - grass
 #define INIT_WORLD_LAYER(value) { [0 ... WORLD_LENGTH-1] = { [0 ... WORLD_BREADTH-1] = value } }
-#define INIT_WORLD(value) { [0 ... WORLD_HEIGHT-1] = INIT_WORLD_LAYER(value) } // implement each layer with different INIT_WORLD_LAYER value
-int world[WORLD_HEIGHT][WORLD_LENGTH][WORLD_BREADTH] = INIT_WORLD(3);
+int world[WORLD_HEIGHT][WORLD_LENGTH][WORLD_BREADTH] = {
+    [0] = INIT_WORLD_LAYER(1), // Bedrock
+    [1] = INIT_WORLD_LAYER(2), // Stone
+    [2] = INIT_WORLD_LAYER(2), // Stone
+    [3] = INIT_WORLD_LAYER(3), // Dirt
+    [4] = INIT_WORLD_LAYER(3), // Dirt
+    [5] = INIT_WORLD_LAYER(4)  // Grass
+};
 
 typedef struct {
 	int x;
@@ -41,11 +42,11 @@ int main(void) {
 	uint8_t isHolding = 0;
 	float holdTime    = 0.0f;
 
-	const float gravity    = 0.8f;
-	const float jumpSpeed  = 5.0f;
-	float verticalVelocity = 0.0f;
-	float groundHeight     = 8.0f;
-	uint8_t isJumping      = 0;
+	const float gravity      = 0.69f;
+	const float jumpSpeed    = 8.0f;
+	float verticalVelocity   = 0.0f;
+	const float groundHeight = 8.0f;
+	uint8_t isJumping        = 0;
 
 	SetTargetFPS(30);
 
@@ -63,18 +64,18 @@ int main(void) {
 					isHolding = 0;
 				}
 			}
-		}
+		} else isHolding = 0;
 
 		// Jump
 		if (IsKeyPressed(KEY_SPACE) && !isJumping) {
             isJumping = 1;
-            verticalVelocity = -jumpSpeed;
+            verticalVelocity = jumpSpeed;
         }
         if (isJumping) {
-            camera.position.y += verticalVelocity;
-            verticalVelocity += gravity;
+            camera.position.y += verticalVelocity * GetFrameTime();
+            verticalVelocity -= gravity;
 
-            if (camera.position.y >= groundHeight) {
+            if (camera.position.y <= groundHeight) {
                 camera.position.y = groundHeight;
                 isJumping = 0;
                 verticalVelocity = 0.0f;
@@ -97,37 +98,36 @@ int main(void) {
 					if(world[depth][length][breath] < 1) continue;
 					Color color;
 					switch(world[depth][length][breath]) {
-						case 1:
-							// Bedrock
-							color = BLACK;
-							break;
-						case 2:
-							// Stone
-							color = GRAY;
-							break;
-						case 3:
-							// Dirt
-							color = BROWN;
-							break;
-						case 4:
-							// Grass
-							color = DARKGREEN;
-							break;
-						default:
-							// Grass
-							color = DARKGREEN;
-							break;
+                    case 1:
+                        // Bedrock
+                        color = BLACK;
+                        break;
+                    case 2:
+                        // Stone
+                        color = GRAY;
+                        break;
+                    case 3:
+                        // Dirt
+                        color = BROWN;
+                        break;
+                    case 4:
+                        // Grass
+                        color = DARKGREEN;
+                        break;
+                    default:
+                        // Grass
+                        color = DARKGREEN;
+                        break;
 					}
 
 					Vector3 pos = (Vector3){ (float)length, (float)depth, (float)breath };
 					const float size = 1.0f;
 
-
 					DrawCube(pos, size, size, size, color);
 
 					RayCollision col = GetRayCollisionBox(crosshairRay,
-							(BoundingBox){(Vector3){ pos.x - size/4, pos.y - size/4, pos.z - size/4 },
-							(Vector3){ pos.x + size/4, pos.y + size/4, pos.z + size/4 }});
+                                                          (BoundingBox){(Vector3){ pos.x - size/4, pos.y - size/4, pos.z - size/4 },
+                                                                        (Vector3){ pos.x + size/4, pos.y + size/4, pos.z + size/4 }});
 
 					if(col.hit) {
 						selected.x = length;
@@ -148,9 +148,9 @@ int main(void) {
 		DrawLine(width / 2, height / 2 - 10, width / 2, height / 2 + 10, RED);
 
 		// Debug
-		DrawText(TextFormat("Selected: %d, %d, %d", selected.x, selected.y, selected.z), 20, 20, 15, BLACK);
-		DrawText(TextFormat("Camera position: %.2f, %.2f, %.2f", camera.position.x, camera.position.y, camera.position.z), 20, 40, 15, BLACK);
-		DrawText(TextFormat("Camera target: %.2f, %.2f, %.2f", camera.target.x, camera.target.y, camera.target.z), 20, 60, 15, BLACK);
+		// DrawText(TextFormat("Selected: %d, %d, %d", selected.x, selected.y, selected.z), 20, 20, 15, BLACK);
+		// DrawText(TextFormat("Camera position: %.2f, %.2f, %.2f", camera.position.x, camera.position.y, camera.position.z), 20, 40, 15, BLACK);
+		// DrawText(TextFormat("Camera target: %.2f, %.2f, %.2f", camera.target.x, camera.target.y, camera.target.z), 20, 60, 15, BLACK);
 		// DrawText(TextFormat("isHolding: %d", isHolding), 20, 40, 15, BLACK);
 		// DrawText(TextFormat("isHolding: %.2f", holdTime), 20, 60, 15, BLACK);
 
